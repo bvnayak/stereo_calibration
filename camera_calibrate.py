e7f9328017d6373c50444406ab03730bc65250e5
Bhavin Nayak <bhavin.nayak@asu.edu>
2016-04-05 16:16:08 -0700

    Initial commit.

---
+++ camera_calibrate.py
@@ -0,0 +1,108 @@
+import numpy as np
+import cv2
+import glob
+import argparse
+
+
+class StereoCalibration(object):
+    def __init__(self, filepath):
+        # termination criteria
+        self.criteria = (cv2.TERM_CRITERIA_EPS +
+                         cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        self.criteria_cal = (cv2.TERM_CRITERIA_EPS +
+                             cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-5)
+
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        self.objp = np.zeros((9*6, 3), np.float32)
+        self.objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+        # Arrays to store object points and image points from all the images.
+        self.objpoints = []  # 3d point in real world space
+        self.imgpoints_l = []  # 2d points in image plane.
+        self.imgpoints_r = []  # 2d points in image plane.
+
+        self.cal_path = filepath
+        self.read_images(self.cal_path)
+
+    def read_images(self, cal_path):
+        images_right = glob.glob(cal_path + 'RIGHT/*.JPG')
+        images_left = glob.glob(cal_path + 'LEFT/*.JPG')
+
+        for i, fname in enumerate(images_right):
+            img_l = cv2.imread(images_left[i])
+            img_r = cv2.imread(images_right[i])
+
+            gray_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2GRAY)
+            gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+
+            # Find the chess board corners
+            ret_l, corners_l = cv2.findChessboardCorners(gray_l, (9, 6), None)
+            ret_r, corners_r = cv2.findChessboardCorners(gray_r, (9, 6), None)
+
+            # If found, add object points, image points (after refining them)
+            self.objpoints.append(self.objp)
+
+            if ret_l is True:
+                rt = cv2.cornerSubPix(gray_l, corners_l, (11, 11),
+                                      (-1, -1), self.criteria)
+                self.imgpoints_l.append(corners_l)
+
+                # Draw and display the corners
+                ret_l = cv2.drawChessboardCorners(img_l, (9, 6),
+                                                  corners_l, ret_l)
+                cv2.imshow(images_left[i], img_l)
+                cv2.waitKey(500)
+
+            if ret_r is True:
+                rt = cv2.cornerSubPix(gray_r, corners_r, (11, 11),
+                                      (-1, -1), self.criteria)
+                self.imgpoints_r.append(corners_r)
+
+                # Draw and display the corners
+                ret_r = cv2.drawChessboardCorners(img_r, (9, 6),
+                                                  corners_r, ret_r)
+                cv2.imshow(images_right[i], img_r)
+                cv2.waitKey(500)
+            img_shape = gray_l.shape[::-1]
+
+        self.stereo_calibrate(img_shape)
+
+    def stereo_calibrate(self, dims):
+        flags = 0
+        # flags |= cv2.CALIB_FIX_INTRINSIC
+        # flags |= cv2.CALIB_FIX_PRINCIPAL_POINT
+        # flags |= cv2.CALIB_USE_INTRINSIC_GUESS
+        flags |= cv2.CALIB_FIX_FOCAL_LENGTH
+        # flags |= cv2.CALIB_FIX_ASPECT_RATIO
+        flags |= cv2.CALIB_ZERO_TANGENT_DIST
+        # flags |= cv2.CALIB_RATIONAL_MODEL
+        # flags |= cv2.CALIB_SAME_FOCAL_LENGTH
+        flags |= cv2.CALIB_FIX_K3
+        flags |= cv2.CALIB_FIX_K4
+        flags |= cv2.CALIB_FIX_K5
+
+        ret, M1, d1, M2, d2, R, T, E, F = cv2.stereoCalibrate(self.objpoints,
+                                                              self.imgpoints_l,
+                                                              self.imgpoints_r,
+                                                              dims, criteria=self.criteria_cal,
+                                                              flags=flags)
+
+        print('Intrinsic_mtx_1', M1)
+        print('dist_1', d1)
+        print('Intrinsic_mtx_2', M2)
+        print('dist_2', d2)
+
+        print('R', R)
+        print('T', T)
+        print('E', E)
+        print('F', F)
+
+        print('')
+
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='String Filepath')
+    args = parser.parse_args()
+    cal_data = StereoCalibration(args.filepath)
